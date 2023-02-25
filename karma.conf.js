93f8d188ca7ba5791c3c3981fcb2dfe7dafb9572
xConde <edjconde@live.com>
2023-02-25 16:47:57 -0600

    add .nvmrc

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/fish'),
+      dir: require('path').join(__dirname, './coverage/novarise'),
       subdir: '.',
       reporters: [
         { type: 'html' },
