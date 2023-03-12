77b2e0f0d3cc3862cffd38834400aad5e9a7e367
xConde <edjconde@live.com>
2023-03-12 14:35:59 -0500

    update game component design w/interactive title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Game - Novarise'`, () => {
+  it(`should have as title 'Novarise'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Game - Novarise');
+    expect(app.title).toEqual('Novarise');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Game - Novarise app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Novarise app is running!');
   });
 });
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+  styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
+  title = 'Novarise';
+  hoverIndex: number | null = null;
 
   constructor() { }
 
