4186f93bc46b63d08e5adfb770dc10e9cc5fd604
xConde <edjconde@live.com>
2023-03-13 14:41:05 -0500

    add cheat code validation

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -9,7 +9,13 @@ export class GameComponent implements OnInit {
   title = 'Novarise';
   hoverIndex: number | null = null;
   ccSession = false;
-  arrows: string[] = [];
+  ccCompleteStatus: 'fail' | 'success' | '';
+  ccArrows: string[] = [];
+  ccs: { code: string[], successMessage: string }[] = [
+    { code: ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right'], successMessage: 'Success!' },
+    { code: ['up', 'up', 'down', 'down', 'left', 'right', 'left'], successMessage: 'Success!' },
+    { code: ['up', 'up', 'down', 'down', 'left', 'right'], successMessage: 'Success!' }
+  ];
 
   constructor() { }
 
@@ -17,22 +23,44 @@ export class GameComponent implements OnInit {
   }
 
   isClicked() {
+    if (this.ccCompleteStatus) { return; }
     this.ccSession = !this.ccSession;
     this.resetCCSession();
   }
 
   resetCCSession() {
-    if (this.arrows.length === 8) { setTimeout(() => { this.ccSession = false; this.arrows = []; }, 1000); }
-    if (!this.ccSession) { this.arrows = []; }
+    if (this.ccArrows.length === 8) {
+      this.checkCheatCodes();
+    }
+    if (!this.ccSession) { this.ccArrows = []; }
+  }
+
+  checkCheatCodes() {
+    const matchingCheatCodes = this.ccs
+      .filter(code => code.code.length <= this.ccArrows.length)
+      .filter(code => code.code.every((val, i) => val === this.ccArrows[i]));
+
+    this.ccCompleteStatus = matchingCheatCodes.length > 0 ? 'success' : 'fail';
+    setTimeout(() => {
+      this.ccSession = false;
+      this.ccArrows = [];
+      this.ccCompleteStatus = '';
+    }, 4000);
   }
 
   @HostListener('window:keydown', ['$event'])
   onArrowKeydown(event: KeyboardEvent) {
-    if (!this.ccSession && this.arrows.length > 7) { return; }
+    if (!this.ccSession && this.ccArrows.length > 7) { return; }
       const arrowKeyCodes = [37, 38, 39, 40];
       if (arrowKeyCodes.includes(event.keyCode)) {
+        const arrowKeyMap: { [key: string]: string } = {
+          'ArrowUp': 'up',
+          'ArrowDown': 'down',
+          'ArrowLeft': 'left',
+          'ArrowRight': 'right'
+        };
         event.preventDefault();
-        this.arrows.push(event.key);
+        this.ccArrows.push(arrowKeyMap[event.key]);
     }
     this.resetCCSession();
   }
