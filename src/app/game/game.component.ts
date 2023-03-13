9b2dca148091de891aa11d1d42fa2cfb2402c003
xConde <edjconde@live.com>
2023-03-13 12:56:42 -0500

    add ccSession design

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-game',
@@ -8,11 +8,33 @@ import { Component, OnInit } from '@angular/core';
 export class GameComponent implements OnInit {
   title = 'Novarise';
   hoverIndex: number | null = null;
-  isClicked = false;
+  ccSession = false;
+  arrows: string[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  isClicked() {
+    this.ccSession = !this.ccSession;
+    this.resetCCSession();
+  }
+
+  resetCCSession() {
+    if (this.arrows.length === 8) { setTimeout(() => { this.ccSession = false; this.arrows = []; }, 1000); }
+    if (!this.ccSession) { this.arrows = []; }
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  onArrowKeydown(event: KeyboardEvent) {
+    if (!this.ccSession && this.arrows.length > 7) { return; }
+      const arrowKeyCodes = [37, 38, 39, 40];
+      if (arrowKeyCodes.includes(event.keyCode)) {
+        event.preventDefault();
+        this.arrows.push(event.key);
+    }
+    this.resetCCSession();
   }
 
 }
